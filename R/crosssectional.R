a778ec1fadd6ebfdb0e65a5834967f0b9be422d8
QinyunLin <linqinyu@msu.edu>
2019-02-25 16:04:16 -0500

    Fix the problem with gather
    Previously the gather brings a problem for global variable undefined. Now it is fixed by generating a long format result and not using gather function.

--- R/crosssectional.R
+++ R/crosssectional.R
@@ -11,15 +11,14 @@
 #' @return plot for how rxmu affects the percentage of bias in cross-sectional model
 #' @import ggplot2
 #' @import scales
-#' @import tidyr
 #' @export
 rxmu <- function(rxmo=-2,rxy=-2,rymo=-2,rmomu=-2,rymu=-2){
   #check input
   if (!(check_input(rxmo) && check_input(rxy) && check_input(rymo) && check_input(rmomu) && check_input(rymu))) {
     return("Error: input is problematic!")
   }
-  result <- matrix(ncol=4)
-  colnames(result) <- c("Bias_a1","Bias_b1","Bias_c","rxmu")
+  result_a1 <- result_b1 <- result_c <- matrix(ncol = 3)
+  colnames(result_a1) <- colnames(result_b1) <- colnames(result_c) <- c("rxmu","Bias","Parameter")
   #return("Normal")
   for (i in -22.5 : 22.5) {
     rxmu <- i/25
@@ -35,12 +34,19 @@ rxmu <- function(rxmo=-2,rxy=-2,rymo=-2,rmomu=-2,rymu=-2){
       bias_a1 <- cal_bias_a1_perc(rxmo,rxmu,rmomu)
       bias_b1 <- cal_bias_b1_perc(rxmo,rxmu,rmomu,rxy,rymo,rymu)
       bias_c <- cal_bias_c_perc(rxmo,rxmu,rmomu,rxy,rymo,rymu)
-      result <- rbind(result,c(bias_a1,bias_b1,bias_c,rxmu))
+      result_a1 <- rbind(result_a1,c(rxmu,bias_a1, "bias_a1"))
+      result_b1 <- rbind(result_b1,c(rxmu,bias_b1,"bias_b1"))
+      result_c <- rbind(result_c,c(rxmu,bias_c,"bias_c"))
     }
   }
-  result.wide <- as.data.frame(result[2:nrow(result),])
-  result.long <- gather(result.wide,Parameter,perc_bias,Bias_a1:Bias_c,factor_key = TRUE)
-  figure <- ggplot(result.long, (aes(rxmu, perc_bias, linetype = Parameter,colour = Parameter,shape = Parameter))) +
+  result_a1 <- as.data.frame(result_a1[2:nrow(result_a1),])
+  result_b1 <- as.data.frame(result_b1[2:nrow(result_b1),])
+  result_c <- as.data.frame(result_c[2:nrow(result_c),])
+  result <- rbind(result_a1,result_b1,result_c)
+  result$Parameter <- as.factor(result$Parameter)
+  result$rxmu <- as.numeric(as.character(result$rxmu))
+  result$Bias <- as.numeric(as.character(result$Bias))
+  figure <- ggplot(result, (aes(rxmu, result$Bias, linetype = result$Parameter,colour = result$Parameter,shape = result$Parameter))) +
     geom_line() +
     geom_point() +
     scale_y_continuous(labels = scales::percent_format(accuracy = 1))+
@@ -64,15 +70,14 @@ rxmu <- function(rxmo=-2,rxy=-2,rymo=-2,rmomu=-2,rymu=-2){
 #' @return plot for how rmomu affects the percentage of bias in cross-sectional model
 #' @import ggplot2
 #' @import scales
-#' @import tidyr
 #' @export
 rmomu <- function(rxmo=-2,rxy=-2,rymo=-2,rxmu=-2,rymu=-2){
   #check input
   if (!(check_input(rxmo) && check_input(rxy) && check_input(rymo) && check_input(rxmu) && check_input(rymu))) {
     return("Error: input is problematic!")
   }
-  result <- matrix(ncol=4)
-  colnames(result) <- c("Bias_a1","Bias_b1","Bias_c","rmomu")
+  result_a1 <- result_b1 <- result_c <- matrix(ncol = 3)
+  colnames(result_a1) <- colnames(result_b1) <- colnames(result_c) <- c("rmomu","Bias","Parameter")
   #return("Normal")
   for (i in -22.5 : 22.5) {
     rmomu <- i/25
@@ -88,12 +93,19 @@ rmomu <- function(rxmo=-2,rxy=-2,rymo=-2,rxmu=-2,rymu=-2){
       bias_a1 <- cal_bias_a1_perc(rxmo,rxmu,rmomu)
       bias_b1 <- cal_bias_b1_perc(rxmo,rxmu,rmomu,rxy,rymo,rymu)
       bias_c <- cal_bias_c_perc(rxmo,rxmu,rmomu,rxy,rymo,rymu)
-      result <- rbind(result,c(bias_a1,bias_b1,bias_c,rmomu))
+      result_a1 <- rbind(result_a1,c(rmomu,bias_a1, "bias_a1"))
+      result_b1 <- rbind(result_b1,c(rmomu,bias_b1,"bias_b1"))
+      result_c <- rbind(result_c,c(rmomu,bias_c,"bias_c"))
     }
   }
-  result.wide <- as.data.frame(result[2:nrow(result),])
-  result.long <- gather(result.wide,Parameter,perc_bias,Bias_a1:Bias_c,factor_key = TRUE)
-  figure <- ggplot(result.long, (aes(rmomu,perc_bias, linetype = Parameter,colour = Parameter,shape = Parameter))) +
+  result_a1 <- as.data.frame(result_a1[2:nrow(result_a1),])
+  result_b1 <- as.data.frame(result_b1[2:nrow(result_b1),])
+  result_c <- as.data.frame(result_c[2:nrow(result_c),])
+  result <- rbind(result_a1,result_b1,result_c)
+  result$Parameter <- as.factor(result$Parameter)
+  result$rmomu <- as.numeric(as.character(result$rmomu))
+  result$Bias <- as.numeric(as.character(result$Bias))
+  figure <- ggplot(result, (aes(rmomu,result$Bias, linetype = result$Parameter,colour = result$Parameter,shape = result$Parameter))) +
     geom_line() +
     geom_point() +
     scale_y_continuous(labels = scales::percent_format(accuracy = 1))+
@@ -117,15 +129,14 @@ rmomu <- function(rxmo=-2,rxy=-2,rymo=-2,rxmu=-2,rymu=-2){
 #' @return plot for how rmomu affects the percentage of bias in cross-sectional model
 #' @import ggplot2
 #' @import scales
-#' @import tidyr
 #' @export
 rymu <- function(rxmo=-2,rxy=-2,rymo=-2,rxmu=-2,rmomu=-2){
   #check input
   if (!(check_input(rxmo) && check_input(rxy) && check_input(rymo) && check_input(rxmu) && check_input(rmomu))) {
     return("Error: input is problematic!")
   }
-  result <- matrix(ncol=4)
-  colnames(result) <- c("Bias_a1","Bias_b1","Bias_c","rymu")
+  result_a1 <- result_b1 <- result_c <- matrix(ncol = 3)
+  colnames(result_a1) <- colnames(result_b1) <- colnames(result_c) <- c("rymu","Bias","Parameter")
   #return("Normal")
   for (i in -22.5 : 22.5) {
     rymu <- i/25
@@ -141,12 +152,19 @@ rymu <- function(rxmo=-2,rxy=-2,rymo=-2,rxmu=-2,rmomu=-2){
       bias_a1 <- cal_bias_a1_perc(rxmo,rxmu,rmomu)
       bias_b1 <- cal_bias_b1_perc(rxmo,rxmu,rmomu,rxy,rymo,rymu)
       bias_c <- cal_bias_c_perc(rxmo,rxmu,rmomu,rxy,rymo,rymu)
-      result <- rbind(result,c(bias_a1,bias_b1,bias_c,rymu))
+      result_a1 <- rbind(result_a1,c(rymu,bias_a1, "bias_a1"))
+      result_b1 <- rbind(result_b1,c(rymu,bias_b1,"bias_b1"))
+      result_c <- rbind(result_c,c(rymu,bias_c,"bias_c"))
     }
   }
-  result.wide <- as.data.frame(result[2:nrow(result),])
-  result.long <- gather(result.wide,Parameter,perc_bias,Bias_a1:Bias_c,factor_key = TRUE)
-  figure <- ggplot(result.long, (aes(rymu, perc_bias, linetype = Parameter,colour = Parameter,shape = Parameter))) +
+  result_a1 <- as.data.frame(result_a1[2:nrow(result_a1),])
+  result_b1 <- as.data.frame(result_b1[2:nrow(result_b1),])
+  result_c <- as.data.frame(result_c[2:nrow(result_c),])
+  result <- rbind(result_a1,result_b1,result_c)
+  result$Parameter <- as.factor(result$Parameter)
+  result$rymu <- as.numeric(as.character(result$rymu))
+  result$Bias <- as.numeric(as.character(result$Bias))
+  figure <- ggplot(result, (aes(rymu, result$Bias, linetype = result$Parameter,colour = result$Parameter,shape = result$Parameter))) +
     geom_line() +
     geom_point() +
     scale_y_continuous(labels = scales::percent_format(accuracy = 1))+

4248dca53f6f419f666287767012fce31ea21535
QinyunLin <linqinyu@msu.edu>
2020-03-18 13:45:45 -0400

    update with a1b1

--- shiny/test.R
+++ shiny/test.R
@@ -19,14 +19,17 @@ ui <- fluidPage(
                    value = 0.95, min = 0.01, max = 0.99, step = 0.01),
       radioButtons(inputId ="labelest",
                    label = "Parameter of interest",
-                   choices = c("a1","b1", "c","a1b1indirect1"))),
+                   choices = c("a1","b1", "c","a1b1", "a1,b1, and a1b1"))),
   mainPanel(plotOutput("plot", width = "100%"))
   )
   )
 server <- function(input, output){
-  output$plot <- renderPlot({
-    rxmu_plot(input$rxmo, input$rxy, input$rymo, input$nobs,
-              input$labelest, input$conflevel, specifyunob = 0)
-    }, width = 600, height = 800)
+  output$plot <- renderPlot(
+    {if (input$labelest=="a1,b1, and a1b1")
+      {rxmu_plot_a1b1(input$rxmo, input$rxy, input$rymo, input$nobs,
+                     input$conflevel, specifyunob = 0)}
+    else {rxmu_plot(input$rxmo, input$rxy, input$rymo, input$nobs,input$labelest,
+               input$conflevel, specifyunob = 0)}}, width = 600, height = 800)
 }
+
 shinyApp(ui = ui, server = server)
